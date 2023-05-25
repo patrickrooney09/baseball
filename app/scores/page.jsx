00fb198e0364f1680558fedb2bd9ed92e7c7a360
Patrick Rooney <patrickrooney09@gmail.com>
2023-05-25 17:05:02 -0400

    implementing data fetching for scores

--- app/scores/page.jsx
+++ app/scores/page.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
 const Scores = () => {
+  async function getData() {
+    let response = fetch(
+      `https://api.mysportsfeeds.com/v2.1/pull/mlb/2023-regular/date/20230523/games.json`,
+      {
+        method: "GET",
+        headers: {
+          Authorization:
+            "Basic " + btoa(`86d6957a-ebe5-48a6-8a41-d69844:MYSPORTSFEEDS`),
+        },
+      }
+    );
+    let data = await response.json();
+    console.log(data);
+    return data;
+  }
+
+  getData();
+
   return (
     <div>
       <h1>Todays Scores</h1>
